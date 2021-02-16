d55779778b73f9bc6cb7f03a3e1c98b1729ad6a8
Gavin Stark <gjstark@me.com>
2021-02-16 18:50:36 +0000

    [rules] Added arbitrary sized BitMask

--- stylesheet/src/tree_rules/bitmask.rs
+++ stylesheet/src/tree_rules/bitmask.rs
@@ -32,6 +32,8 @@ pub trait BitMask : Sized + std::fmt::Debug {
     fn is_set(&mut self, n:usize) -> bool;
 }
 
+
+//a BitMaskU
 //tp BitMaskU - a u8/u16/u32/u64 that has the trait BitMask
 /// This struct is a generic implementation of BitMask using a single u<>
 pub struct BitMaskU<U> where U:num::Unsigned {
@@ -104,42 +106,59 @@ pub type BitMaskU32 = BitMaskU<u64>;
 impl Maskable<u64> for u64 {}
 pub type BitMaskU64 = BitMaskU<u64>;
 
-//a Rules
-//tp RuleResult
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum RuleResult {
-    /// Indicate that the rule does not match and it should not propagate in to children
-    MismatchEnd,
-    /// Indicate that the rule does not match but it should propagate in to children
-    MismatchPropagate,
-    /// Indicate that the rule matched and it should not propagate in to children
-    /// A match indicates that the rule action fires and subrules are enabled for children
-    MatchEndChildren,
-    /// Indicate that the rule matched and it should propagate in to children
-    /// A match indicates that the rule action fires and subrules are enabled for children
-    MatchPropagateChildren,
-    /// Indicate that the rule matched and it should not propagate in to children
-    /// A match indicates that the rule action fires and subrules are run on for the node
-    MatchEndAgain,
-    /// Indicate that the rule matched and it should propagate in to children
-    /// A match indicates that the rule action fires and subrules are run on for the node
-    MatchPropagateAgain,
+
+
+//a BitMaskX
+//tp BitMaskX - an arbitrarily-sized vector
+/// This supports arbitrarily-sized vectors
+///
+/// It utilizes a vector of u64, so it is should be used only for
+/// where more than 64 bits are required
+#[derive(Debug)]
+pub struct BitMaskX {
+    mask : Vec<u64>,
 }
 
-//ip std::fmt::Display for RuleResult
-impl std::fmt::Display for RuleResult {
-    //mp fmt
-    /// Display the rule
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            Self::MismatchEnd        => write!(f, "mismatch"),
-            Self::MismatchPropagate  => write!(f, "mismatch but apply rule to children"),
-            Self::MatchEndChildren        => write!(f, "*match*"),
-            Self::MatchPropagateChildren  => write!(f, "*match* and also apply rule to children"),
-            Self::MatchEndAgain           => write!(f, "*match* and apply child rules to this node"),
-            Self::MatchPropagateAgain     => write!(f, "*match* and apply child rules only to this node and this rule to children"),
-        }
+//ip BitMask for BitMaskX
+/// Implementation of BitMask for an array of unsigned values used as a bit mask
+impl BitMask for BitMaskX{
+    //fp new
+    /// Create a new mask
+    #[inline]
+    fn new(n:usize) -> Self {
+        let num_u64 = (n+63)/64;
+        let mut mask = Vec::with_capacity(num_u64);
+        for _ in 0..num_u64 { mask.push(0); }
+        Self { mask }
+    }
+
+    #[inline]
+    fn clone(&self, n:usize) -> Self {
+        let num_u64 = (n+63)/64;
+        let mut mask = Vec::with_capacity(num_u64);
+        for i in 0..num_u64 { mask.push(self.mask[i]); }
+        Self { mask }
+    }
+
+    #[inline]
+    fn set(&mut self, n:usize) {
+        let b = n % 64;
+        let n = n / 64;
+        self.mask[n] = self.mask[n] | (1<<b);
+    }
+
+    #[inline]
+    fn clear(&mut self, n:usize) {
+        let b = n % 64;
+        let n = n / 64;
+        self.mask[n] = self.mask[n] & !(1<<b);
+    }
+
+    #[inline]
+    fn is_set(&mut self, n:usize) -> bool {
+        let b = n % 64;
+        let n = n / 64;
+        (self.mask[n] >> b) & 1 == 1
     }
 }
-
 
